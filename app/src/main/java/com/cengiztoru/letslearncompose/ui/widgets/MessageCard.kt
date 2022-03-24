4721b06fa891db99adee6bfd5f379c050ced9334
Cengiz TORU <cengiztoru@gmail.com>
2022-03-24 15:17:57 +0300

    Implemented material design section

--- app/src/main/java/com/cengiztoru/letslearncompose/ui/widgets/MessageCard.kt
+++ app/src/main/java/com/cengiztoru/letslearncompose/ui/widgets/MessageCard.kt
@@ -1,12 +1,17 @@
 package com.cengiztoru.letslearncompose.ui.widgets
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,16 +21,20 @@ import com.cengiztoru.letslearncompose.data.model.Message
 
 @Composable
 fun MessageCard(msg: Message) {
-    Row(modifier = Modifier.padding(8.dp)) {
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .border(0.dp, Color.Transparent)
+            .padding(8.dp)
+    ) {
 
         Image(
             painter = painterResource(id = R.drawable.baby),
             contentDescription = "Contact profile picture",
             modifier = Modifier
                 .size(80.dp)
-                .clip(
-                    CircleShape
-                ),
+                .clip(CircleShape)
+                .border(2.dp, MaterialTheme.colors.secondary, CircleShape),
             contentScale = ContentScale.FillHeight,
         )
 
@@ -33,9 +42,23 @@ fun MessageCard(msg: Message) {
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
             Spacer(modifier = Modifier.height(8.dp))
-            Text(text = msg.body)
+
+            Surface(
+                modifier = Modifier.fillMaxWidth(),
+                shape = MaterialTheme.shapes.medium,
+                elevation = 10.dp,
+                border = BorderStroke(0.dp, MaterialTheme.colors.secondary)
+            ) {
+                Text(
+                    text = msg.body, modifier = Modifier.padding(16.dp), style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
 }
