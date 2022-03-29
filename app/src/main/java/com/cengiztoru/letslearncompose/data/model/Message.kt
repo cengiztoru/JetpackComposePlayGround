ea959879048ab11eb42f1fb1fbea2c60a501cb78
Cengiz TORU <cengiztoru@gmail.com>
2022-03-29 16:48:33 +0300

    A new variable added to hold message expand status

--- app/src/main/java/com/cengiztoru/letslearncompose/data/model/Message.kt
+++ app/src/main/java/com/cengiztoru/letslearncompose/data/model/Message.kt
@@ -1,3 +1,3 @@
 package com.cengiztoru.letslearncompose.data.model
 
-data class Message(val author: String, val body: String)
+data class Message(val author: String, val body: String, var isExpanded: Boolean = false)
