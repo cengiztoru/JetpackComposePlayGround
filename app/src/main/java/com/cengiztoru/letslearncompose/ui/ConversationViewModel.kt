99e03883dd685057badfc480aa4cfb722d95ff87
Cengiz TORU <cengiztoru@gmail.com>
2022-03-29 17:03:00 +0300

    Created a viewmodel to hold messages and include item expand status business logic

---
+++ app/src/main/java/com/cengiztoru/letslearncompose/ui/ConversationViewModel.kt
@@ -0,0 +1,23 @@
+package com.cengiztoru.letslearncompose.ui
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.cengiztoru.letslearncompose.data.model.Message
+import com.cengiztoru.letslearncompose.utils.SampleData
+
+class ConversationViewModel : ViewModel() {
+
+    var messages = mutableStateOf(SampleData.getMockChatList())
+        private set
+
+    fun itemExpandStateChanged(changedItem: Message) {
+        val currentList = messages.value.toMutableList()
+        currentList.forEachIndexed { index, item ->
+            if (changedItem == item) {
+                currentList[index] = changedItem.copy(isExpanded = changedItem.isExpanded.not())
+            }
+        }
+        messages.value = currentList.toList()
+    }
+
+}
