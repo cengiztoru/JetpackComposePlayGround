63373360f9319484a7058d3ecae9ef451e8fcebc
Cengiz TORU <cengiztoru@gmail.com>
2022-03-29 17:07:30 +0300

    The conversations are shown by observing list from viewmodel in order to store item expand status while the user scrolls the items. And also saving items state if will be any configuration changes

--- app/src/main/java/com/cengiztoru/letslearncompose/ui/widgets/LetsLearnCompose.kt
+++ app/src/main/java/com/cengiztoru/letslearncompose/ui/widgets/LetsLearnCompose.kt
@@ -4,8 +4,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.cengiztoru.letslearncompose.ui.ConversationViewModel
 import com.cengiztoru.letslearncompose.ui.theme.LetsLearnComposeTheme
 import com.cengiztoru.letslearncompose.utils.SampleData
 
@@ -14,7 +18,13 @@ fun LetsLearnCompose() {
     LetsLearnComposeTheme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-            Conversation(messages = SampleData.getMockChatList())
+            val viewModel = viewModel<ConversationViewModel>()
+
+            val lazyListState by remember { viewModel.messages }
+
+            Conversation(messages = lazyListState) {
+                viewModel.itemExpandStateChanged(it)
+            }
         }
     }
 
@@ -24,5 +34,7 @@ fun LetsLearnCompose() {
 
 @Composable
 fun LetsLearnComposePreview() {
-    Conversation(messages = SampleData.getMockChatList())
+    Conversation(messages = SampleData.getMockChatList()) {
+
+    }
 }
